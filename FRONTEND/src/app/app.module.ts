8ac022d316da5f017b3ddb997c1fc67d48cca73c
saabirah <saabirah1993@gmail.com>
2021-06-02 15:43:46 +0100

    install bootstrap, j-query, popper, create register, login, header, footer and nav component

--- FRONTEND/src/app/app.module.ts
+++ FRONTEND/src/app/app.module.ts
@@ -2,14 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+import { NgxPopper } from 'angular-popper';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    HeaderComponent,
+    MainComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
+    NgxPopper,
     AppRoutingModule
   ],
   providers: [],
